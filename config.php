efab94b50d784429a55f9b47209701bdf471a2d0
Laboudigue quentin <qlaboudigue@gmail.com>
2022-02-16 15:29:31 +0100

    Config review

--- config.php
+++ config.php
@@ -5,10 +5,10 @@
 
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', '127.0.0.1');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'password');
-define('DB_NAME', 'pawn');
+define('DB_SERVER', '????');
+define('DB_USERNAME', '????');
+define('DB_PASSWORD', '????');
+define('DB_NAME', '????');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
